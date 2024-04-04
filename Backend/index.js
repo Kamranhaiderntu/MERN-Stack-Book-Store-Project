5e978de6986d9f623c0f8603d18226e2dba0b8f4
Kamran Haider <19ntucs1146@student.ntu.edu.pk>
2024-04-04 23:02:02 +0500

    new

--- Backend/index.js
+++ Backend/index.js
@@ -39,7 +39,7 @@ app.post('/books', async (request, response) => {
 
 mongoose.connect(MONGO_URI)
     .then(() => {
-        console.log("App Connectedd to MongoDB Atlas Database Database");
+        console.log("App Connectedd to MongoDB Atlas Database");
         app.listen(PORT, () => {
             console.log(`Server is running on port:${PORT}`);
         });
